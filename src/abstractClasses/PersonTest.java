92a44d79ecf7e3fe500e5072212f435a120216ec
rusliksu <gayanov96@mail.ru>
2018-10-31 02:17:12 +0300

    StaticTest

---
+++ src/abstractClasses/PersonTest.java
@@ -0,0 +1,21 @@
+package abstractClasses;
+
+/**
+ * В этой программе демонстрируется применение абстрактных классов
+ */
+
+public class PersonTest {
+    public static void main(String[] args)
+    {
+        Person[] people = new Person[2];
+
+        // заполнить массив people объектами типа Student и Employee
+        people[0] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+        people[1] = new Student("Maria Morris", "computer science");
+
+        // вывести имена и описания всех лиц,
+        // представленных объектами типа Person
+        for (Person p : people)
+            System.out.println(p.getName() + ", " + p.getDescription());
+    }
+}
---
+++ src/abstractClasses/Student.java
@@ -0,0 +1,22 @@
+package abstractClasses;
+
+public class Student extends Person{
+    private String major;
+
+    /**
+     * @param name Имя студента
+     * @param major Специализация студента
+     */
+
+    public Student(String name, String major) {
+        // передать строку name конструктору суперкласса
+        // в качестве его параметра
+        super(name);
+        this.major = major;
+    }
+
+    @Override
+    public String getDescription() {
+        return "a student majoring in " + major;
+    }
+}
